ab30286220221be0db7d41d2ed001952f8defc80
rjswh0503 <rjswh0503@yahoo.com>
2023-12-19 12:13:49 +0900

    todoList

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import './App.css';
 //class App extends Component{}
 
 
-//onst App = () => {}
+//const App = () => {}
 
 
 
 const App = () => {
-  
+
 
   // todos의 초기값을 빈 배열로 생성하겠다는 의미
 
@@ -42,8 +42,14 @@ const App = () => {
     const updateTodos = [...todos];
     // 복사된 배열에서 지정된 자리값(index)를 1개 제거하겠다는 의미
     //updateTodos : 복사된 배열
-    // slice : 제거
-    updateTodos.slice(index, 1);
+    // slice : 배열 일부분 복사해서 새로운 배열 반환
+    // 원본에 있던 배열은 변경되지 않지만 시작-종료 인덱스 
+
+    // splice :  배열에 내용을 수정하거나 삭제하고, 필요에 따라 새로운 요소를 추가할 수 있음 
+    // slice splice : 배열을 조작
+    //파이썬에서 많이 씀
+
+      updateTodos.splice(index, 1);
     //내가 제거하고 싶은 할 일을 제거한 후
     //setTodos를 활용해서 할 일 목록을 재설정
     setTodos(updateTodos);
@@ -53,12 +59,13 @@ const App = () => {
     <div>
       <h2>useState를 활용한 TodoList</h2>
       <div>
-        <input 
-        
-        type='text' value={newTodo} onChange={(e) => setNewTodo(e.target.value)}
-        />
-        <button onClick={addTodo}>할 일 추가하기</button>
-      
+        <div className='list'>
+          <input 
+          
+          type='text' value={newTodo} onChange={(e) => setNewTodo(e.target.value)}
+          />
+          <button onClick={addTodo}>할 일 추가하기</button>
+        </div>
       <ul>
         {todos.map((todo, index) => (
           <li key={index}>
@@ -70,6 +77,42 @@ const App = () => {
     </div>
     </div>
   );
-}
+};
 
 export default App;
+
+
+
+
+//my-app
+// map 배열 객체 메서드
+// 배열의 각 요소에서 함수를 호출하고, 그 함수의 반환값으로 새로운 배열을 생성
+
+/*
+ 예제코드 : 
+    const newArray = array.map((value, index, array)=> {});   
+ value : 현재 배열안에 있는 배열의 요소 
+
+ index : 배열안에 있는 자릿값
+
+ array : 배열의 원본
+
+ 반환값{} : 새로운 배열에서 해당하는 인덱스에 들어갈 값
+
+
+{todos.map((todo, index)=() => (
+
+<li key={index}> key={index} map 함수로 요소를 생성할 때 각 요소의 고요한 키 값을 지정
+                                      react가 각 값에 대한 번호를 부여함으로써 목록을 부여된 번호로 추적하기 위해 사용됨
+  {todo}
+  <button onClick={()=> removeTodo(index)}>삭제하기</button>
+  </li>
+)}
+
+
+array.map((요소, 자릿값, 만약에 새로운 배열이 필요하다면 배열 값 넣어줌) => {
+  리턴값으로 새로운 배열을 구성
+})
+
+
+ */
